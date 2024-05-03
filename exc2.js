599245a250887100dc56732847925edea9cc1862
OtavioChiaki <bovolonotavio@gmail.com>
2024-05-03 15:17:13 -0300

    exercicio 2

---
+++ exc2.js
@@ -0,0 +1,23 @@
+let dias = prompt ("informe um número de 1 a 7")
+if (dias == "1") {
+    console.log (" segunda-feira")
+}
+if (dias == "2") {
+    console.log (" terça-feira")
+}
+if (dias == "3") {
+    console.log (" quarta-feira")
+}
+if (dias == "4") {
+    console.log (" quinta-feira")
+}
+if (dias == "5") {
+    console.log (" sexta-feira")
+}
+if (dias == "6") {
+    console.log (" sexta-feira")
+}
+if (dias == "7") {
+    console.log (" sexta-feira")
+}
+
