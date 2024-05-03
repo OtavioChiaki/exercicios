c8bae78255dadf0a2ea3aa1ab6c07014ca3d72e0
OtavioChiaki <bovolonotavio@gmail.com>
2024-05-03 14:58:14 -0300

    feat: exercicio 1

---
+++ exc1.js
@@ -0,0 +1,6 @@
+let user = prompt ("quem é vc")
+let senha = prompt ("qual é sua senha?")
+if (user == "adimin" && senha == "senha123") {
+    console.log ("bem-sucedido")
+}
+else {console.log ("não funfo")}
