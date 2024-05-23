2493f398ce95c8b268e3b33566656fea09bfbb16
OTAVIO BOVOLON ARAUJO <50189957859@info.local>
2024-05-23 20:26:52 -0300

    feat: fim do menuzinho

--- objeto&funcao/exercicioDoMenu.js
+++ objeto&funcao/exercicioDoMenu.js
@@ -18,7 +18,7 @@ function exemploRepeticao() {
     console.log("Saiuuuuu");
 }
 
-
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const exercicioLogin = () => {
     let username = prompt("Digite um nome de usuario:")
     let senha = prompt("Digite a senha:")
@@ -27,10 +27,10 @@ const exercicioLogin = () => {
     if (username == "admin" && senha == "senha123") {
         alert("Login bem sucedido");
     } else {
-        alert("Voce não tem permissão de acesso!")
+        alert("Voce não tem permissão de acesso!  (▀̿̿Ĺ̯̿▀̿ ̿)")
     }
 }
-
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const tabuada = () => {
     let tabuada = prompt("informe um número de 1 a 10 para mostrar sua tabuada ")
 
@@ -41,47 +41,87 @@ const tabuada = () => {
 
     }
 }
-
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const IMC = () => {
     let peso = parseFloat(prompt("Informe o seu peso (em kg): "));
 
-let altura = parseFloat(prompt("Informe o seu altura (em metros): "));
+    let altura = parseFloat(prompt("Informe o seu altura (em metros): "));
 
-let imc = peso / (altura*altura);
+    let imc = peso / (altura * altura);
 
-console.log("Seu IMC é:" + imc.toFixed(2));
+    console.log("Seu IMC é:" + imc.toFixed(2));
 
-if(imc < 18.5){
-    alert("Abaixo do peso");
+    if (imc < 18.5) {
+        alert("Abaixo do peso");
+    }
+
+    else if (imc >= 18.5 && imc <= 24.9) {
+        alert("Peso normal");
+    }
+
+    else if (imc >= 25.0 && imc <= 29.9) {
+        alert("Sobrepeso");
+    }
+
+    else if (imc >= 30.0 && imc <= 34.9) {
+        alert("Obsidade grau 1");
+    }
+
+    else if (imc >= 35.0 && imc <= 39.9) {
+        alert("Obsidade grau 2");
+    }
+
+    else {
+        alert("Obsidade grau 3");
+    }
+
 }
 
-else if( imc>= 18.5 && imc <= 24.9 ){
-    alert("Peso normal");
-}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-else if (imc >= 25.0 && imc <= 29.9){
-    alert("Sobrepeso");
-}
 
-else if (imc >= 30.0 && imc <= 34.9){
-    alert("Obsidade grau 1");
-}
+const maiorOuMenor = () => {
 
-else if (imc >= 35.0 && imc <= 39.9){
-    alert("Obsidade grau 2");
-}
+    let idade = parseInt(prompt("Digite sua idade: "));
 
-else {
-    alert("Obsidade grau 3");
-}
+    while (idade < 18) {
+        idade = parseInt(prompt("Você é menor de idade. Por favor, digite sua idade novamente: "))
+    }
 
+    console.log("Você é maior de idade. Sua idade é: " + idade);
 }
 
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const fibzinho = () => {
+
+    let fib1 = 1;
+    let fib2 = 1;
+
+    alert("Os primeiro 10 números da sequência de Fibonacci são: ")
+    alert(fib1);
+    alert(fib2);
+
+    for (let i = 2; i < 10; i++) {
+        let nextFib = fib1 + fib2;
+        alert(nextFib);
+
+        fib1 = fib2;
+        fib2 = nextFib;
+    }
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const hellou = () => {
+alert (" Hello World")
+
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 let opcaoUsuario;
 
 do {
-    opcaoUsuario = prompt("MENU\n\n1 - Exemplo de Repetição\n2 - Exercicio Usuario Senha\n3 - Exercicio da Tabuada\n4 - Exercicio de IMC\n5 - Exercicio Usuario Senha\n6 - Exercicio Usuario Senha\n\n0 - Digite 0 para sair")
+    opcaoUsuario = prompt("⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n Olaa⠀⠀⠀⢰⣿⡿⠗⠀⠠⠄⡀⠀⠀⠀⠀\n    ⠀⠀⠀⠀⡜⠁⠀⠀⠀⠀⠀⠈⠑⢶⣶⡄\n    ⢀⣶⣦⣸⠀⢼⣟⡇⠀⠀⢀⣀⠀⠘⡿⠃\n    ⠀⢿⣿⣿⣄⠒⠀⠠⢶⡂⢫⣿⢇⢀⠃⠀\n    ⠀⠈⠻⣿⣿⣿⣶⣤⣀⣀⣀⣂⡠⠊⠀⠀\n    ⠀⠀⠀⠃⠀⠀⠉⠙⠛⠿⣿⣿⣧⠀⠀⠀\n    ⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠘⣿⣿⡇⠀⠀\n    ⠀⠀⠀⣷⣄⡀⠀⠀⠀⢀⣴⡟⠿⠃⠀⠀\n    ⠀⠀⠀⢻⣿⣿⠉⠉⢹⣿⣿⠁⠀⠀⠀⠀\n    ⠀⠀⠀⠀⠉⠁⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀\n\n__ ▂ ▃ ▅ ▇ █ MENU █ ▇ ▅ ▃ ▂ __\n\n1 - Exemplo de Repetição ∞\n2 - Exercicio Usuario Senha (⌐■_■)\n3 -Tabuda （ｏ。ｏ；） \n4 - Exercicio de IMC (｀∀´)Ψ\n5 -  Exercicio de maior ou menor idade (~˘▾˘)~\n6 - Exercicio de Fibonacci  (；一_一)\n\n0 - Digite 0 para sair  ಥ_ಥ\n◕‿◕ Digita 7 ai")
 
     if (opcaoUsuario == "0") {
         alert("Voce escolheu sair :(")
@@ -104,8 +144,17 @@ do {
         IMC()
     }
 
+    else if (opcaoUsuario == "5") {
+        maiorOuMenor()
+    }
 
+    else if (opcaoUsuario == "6") {
+        fibzinho()
+    }
 
+    else if (opcaoUsuario == "7") {
+        hellou ()
+    }
 
     else {
         alert("Exercicio inexistente")
